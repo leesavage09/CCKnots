0d727b714097fc069e32ac7b94dfe600d21c931b
leesavage09 <leesavage09@gmail.com>
2022-10-25 11:46:30 +0300

    added orbit camera control

---
+++ src/components/controllers/orbitControl.tsx
@@ -0,0 +1,24 @@
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useState } from "react"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export const OrbitControl = () => {
+    const { camera, gl } = useThree();
+
+    useEffect(
+        () => {
+            const controls = new OrbitControls(camera, gl.domElement);
+
+            controls.minDistance = 10;
+            controls.maxDistance = 100;
+
+            return () => {
+                controls.dispose();
+            };
+        },
+        [camera, gl]
+    );
+    return null;
+
+    return null;
+};
