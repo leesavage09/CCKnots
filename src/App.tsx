c2adfd7be925d388b0d0892d97755752f79bf525
leesavage09 <leesavage09@gmail.com>
2022-11-02 13:52:44 +0200

    updated app to animate bowline

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { hot } from "react-hot-loader";
 import { Canvas } from '@react-three/fiber'
 import { OrbitControl } from "./components/controllers/orbitControl";
+import { Bowline } from "./components/rope/bowline";
 
 const App: React.FC = () => {
+  const [moveTo, setMoveTo] = useState(0)
+
+  const sliderChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setMoveTo((parseInt(evt.target.value) / 1000))
+  }
 
   return (
     <div className="full-height">
-      <Canvas shadows camera={{ fov: 45, position: [25, 5, 25] }}>
+      <Canvas
+        shadows
+        camera={{ fov: 45, position: [0, 0, 50] }}
+      >
         <OrbitControl />
-        <ambientLight intensity={0.2} />
-        <pointLight castShadow position={[10, 10, 10]} intensity={0.9} />
-        <mesh
-          receiveShadow
-          position={[0, 0, 0]}
-        >
-          <boxGeometry args={[10, 10, 10]} />
-          <meshStandardMaterial color={'red'} />
-        </mesh>
+        <ambientLight intensity={0.1} />
+        <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
+        <Bowline animationFrame={moveTo} />
       </Canvas>
-    </div>
+      <input
+        onChange={(evt) => sliderChange(evt)}
+        type="range"
+        min="0"
+        max="1000"
+      />
+    </div >
   )
 }
 
