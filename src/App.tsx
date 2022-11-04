e7fd84a2476f4a7a37ab22a7c69ae5dc641fee23
leesavage09 <leesavage09@gmail.com>
2022-11-04 17:58:55 +0200

    updated ui and added black background

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { hot } from "react-hot-loader";
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControl } from "./components/controllers/orbitControl";
 import { Bowline } from "./components/rope/bowline";
+import { Color } from "three";
+
+export const Environment = () => {
+  const { scene } = useThree();
+
+  useEffect(() => {
+    scene.background = new Color('black');
+  }, [])
+
+  return (
+    null
+  )
+}
+
 
 const App: React.FC = () => {
-  const [moveTo, setMoveTo] = useState(0)
 
-  const sliderChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    setMoveTo((parseInt(evt.target.value) / 1000))
-  }
+  const [slider, setSlider] = useState("1000")
+  const [knotAnimation, setKnotAnimation] = useState(0)
+
+  useEffect(() => {
+    setKnotAnimation((parseInt(slider) / 1000))
+  }, [slider])
 
   return (
     <div className="full-height">
+      <h1>Bowline</h1>
       <Canvas
         shadows
         camera={{ fov: 45, position: [0, 0, 50] }}
       >
+        <Environment />
         <OrbitControl />
-        <ambientLight intensity={0.1} />
+        <ambientLight intensity={0.3} />
         <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-        <Bowline animationFrame={moveTo} />
+        <Bowline animationFrame={knotAnimation} />
       </Canvas>
       <input
-        onChange={(evt) => sliderChange(evt)}
+        className="input"
+        onChange={(e) => setSlider(e.target.value)}
         type="range"
         min="0"
+        value={slider}
         max="1000"
       />
     </div >
