ebcca16f750d9866918af34cd845146b027c8c03
leesavage09 <leesavage09@gmail.com>
2022-11-05 18:17:14 +0200

    useRopeMesh has configurable color and length

--- src/components/rope/useRopeMesh.tsx
+++ src/components/rope/useRopeMesh.tsx
@@ -1,7 +1,7 @@
 import { useLoader } from '@react-three/fiber';
 import { CylinderGeometry, Mesh, MeshStandardMaterial, RepeatWrapping, TextureLoader, Vector2 } from "three";
 
-export const useRopeMesh = () => {
+export const useRopeMesh = (length: number, color: string) => {
     const aoMap = useLoader(TextureLoader, 'textures/rope/aoMap.jpg');
     const roughnessMap = useLoader(TextureLoader, 'textures/rope/roughnessMap.jpg');
     const normalMap = useLoader(TextureLoader, 'textures/rope/normalMap.jpg');
@@ -9,7 +9,7 @@ export const useRopeMesh = () => {
     const map = useLoader(TextureLoader, 'textures/rope/map.jpg');
 
     aoMap.wrapS = roughnessMap.wrapS = normalMap.wrapS = displacementMap.wrapS = map.wrapS = aoMap.wrapT = roughnessMap.wrapT = normalMap.wrapT = displacementMap.wrapT = map.wrapT = RepeatWrapping
-    aoMap.repeat = roughnessMap.repeat = normalMap.repeat = displacementMap.repeat = map.repeat = new Vector2(6, 500)
+    aoMap.repeat = roughnessMap.repeat = normalMap.repeat = displacementMap.repeat = map.repeat = new Vector2(6, length / 4.166)
 
     const material = new MeshStandardMaterial({
         map: map,
@@ -17,10 +17,11 @@ export const useRopeMesh = () => {
         displacementScale: 0.1,
         normalMap: normalMap,
         roughnessMap: roughnessMap,
-        aoMap: aoMap
+        aoMap: aoMap,
+        color: color
     });
 
-    const geometry = new CylinderGeometry(0.4, 0.4, 120, 100, 1000);
+    const geometry = new CylinderGeometry(0.4, 0.4, length, 100, length * 8.333);
     geometry.rotateZ(Math.PI / 2)
 
     const ropeMesh = new Mesh(geometry, material)
