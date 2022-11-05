efe2b1a75c83b6f8086be5f3ce34175cae01424b
leesavage09 <leesavage09@gmail.com>
2022-11-05 12:30:19 +0200

    abstracted three sky component

---
+++ src/components/controllers/sky.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { useThree } from '@react-three/fiber'
+import { Color } from "three";
+
+export const Sky = () => {
+    const { scene } = useThree();
+
+    useEffect(() => {
+        scene.background = new Color('black');
+    }, [])
+
+    return (
+        null
+    )
+}
