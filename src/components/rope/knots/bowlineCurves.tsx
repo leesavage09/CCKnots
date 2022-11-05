6d29d1f02d3b27487c5bd9d367b939b9ffc24b16
leesavage09 <leesavage09@gmail.com>
2022-11-05 14:00:28 +0200

    added 3 keyframes for bowline curve

--- src/components/rope/knots/bowlineCurves.tsx
+++ src/components/rope/knots/bowlineCurves.tsx
@@ -1,151 +1,226 @@
 import { CubicBezierCurve3, CurvePath, Vector3 } from "three"
 
-export const BowlineCurve_0 = new CurvePath<Vector3>();
-
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(-0.931381, 165.787743, -0.300292),
-    new Vector3(-0.832493, 101.565353, -0.535896),
-    new Vector3(-0.762520, 56.121586, -0.702610),
-    new Vector3(-0.678090, 1.288501, -0.903769)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(-0.678090, 1.288501, -0.903769),
-    new Vector3(-0.708932, -1.146280, -0.903769),
-    new Vector3(-2.435531, -1.981714, 1.713394),
-    new Vector3(-0.663353, -2.569478, 2.314160)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(-0.663353, -2.569478, 2.314160),
-    new Vector3(1.129487, -3.164095, 2.921929),
-    new Vector3(3.517528, -1.799262, 1.127216),
-    new Vector3(3.261527, 0.026803, 0.394295)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(3.261527, 0.026803, 0.394295),
-    new Vector3(3.138090, 0.907283, 0.040900),
-    new Vector3(2.001310, 1.130502, 0.424490),
-    new Vector3(1.046556, 1.075342, 0.396701)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(1.046556, 1.075342, 0.396701),
-    new Vector3(-0.368088, 0.993611, 0.355526),
-    new Vector3(-1.456303, 0.449712, 0.270431),
-    new Vector3(-2.029000, -0.845559, 0.208095)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(-2.029000, -0.845559, 0.208095),
-    new Vector3(-4.892487, -7.321909, -0.103589),
-    new Vector3(-5.475488, -19.354998, -0.160722),
-    new Vector3(2.311750, -24.817846, -0.300292)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(2.311750, -24.817846, -0.300292),
-    new Vector3(5.050669, -26.739232, -0.349381),
-    new Vector3(9.112911, -26.292987, -0.446733),
-    new Vector3(10.761617, -23.385043, -0.300292)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(10.761617, -23.385043, -0.300292),
-    new Vector3(16.069047, -14.023939, 0.171125),
-    new Vector3(2.721774, -12.336980, 0.985883),
-    new Vector3(0.718990, 2.266700, 1.223782)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(0.718990, 2.266700, 1.223782),
-    new Vector3(0.494999, 3.899978, 1.250388),
-    new Vector3(0.178306, 4.438339, -1.675336),
-    new Vector3(-0.872541, 4.370861, -1.626841)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(-0.872541, 4.370861, -1.626841),
-    new Vector3(-2.022139, 4.297040, -1.573789),
-    new Vector3(-1.591814, 2.838498, 0.298723),
-    new Vector3(-1.145646, 1.597480, 0.792350)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(-1.145646, 1.597480, 0.792350),
-    new Vector3(0.734124, -3.631097, 2.872066),
-    new Vector3(-0.155911, -4.823557, 0.126288),
-    new Vector3(2.767694, -8.935914, -0.300292)
-));
-BowlineCurve_0.add(new CubicBezierCurve3(
-    new Vector3(2.767694, -8.935914, -0.300292),
-    new Vector3(5.312546, -12.515516, -0.671608),
-    new Vector3(12.045713, -21.700639, -1.914457),
-    new Vector3(14.969318, -25.812996, -2.341036)
-));
-
 export const BowlineCurve_1 = new CurvePath<Vector3>();
 
 BowlineCurve_1.add(new CubicBezierCurve3(
     new Vector3(-0.931381, 165.787743, -0.300292),
-    new Vector3(-0.832493, 101.565353, -0.535896),
-    new Vector3(-0.762520, 56.121586, -0.702610),
-    new Vector3(-0.678090, 1.288501, -0.903769)
+    new Vector3(-0.799157, 101.565353, -0.535896),
+    new Vector3(-0.705596, 56.121586, -0.702610),
+    new Vector3(-0.592703, 1.288501, -0.903769)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(-0.678090, 1.288501, -0.903769),
-    new Vector3(-0.708932, -1.146280, -0.903769),
-    new Vector3(-1.172653, -1.767261, 1.364740),
-    new Vector3(-1.369374, -3.945390, 1.093755)
+    new Vector3(-0.592703, 1.288501, -0.903769),
+    new Vector3(-0.623545, -1.146280, -0.903769),
+    new Vector3(-0.308106, -0.894902, 1.072044),
+    new Vector3(0.776696, -1.241310, 2.314160)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(-1.369374, -3.945390, 1.093755),
-    new Vector3(-1.584748, -6.330033, 0.797077),
-    new Vector3(-1.488205, -6.604461, -2.539531),
-    new Vector3(-1.587753, -9.012863, -2.436510)
+    new Vector3(0.776696, -1.241310, 2.314160),
+    new Vector3(1.307541, -1.410824, 2.921986),
+    new Vector3(2.482193, -1.012912, 2.676041),
+    new Vector3(2.484064, -0.762549, 3.461725)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(-1.587753, -9.012863, -2.436510),
-    new Vector3(-1.621108, -9.819828, -2.401992),
-    new Vector3(-1.675363, -10.052540, -1.703531),
-    new Vector3(-1.650532, -10.603998, -1.112961)
+    new Vector3(2.484064, -0.762549, 3.461725),
+    new Vector3(2.486583, -0.425314, 4.520031),
+    new Vector3(1.187904, 0.054283, 4.897437),
+    new Vector3(0.191857, -0.391061, 5.105533)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(-1.650532, -10.603998, -1.112961),
-    new Vector3(-1.619497, -11.293214, -0.374863),
-    new Vector3(-1.630887, -11.215995, 0.562618),
-    new Vector3(-1.414637, -12.111657, 0.977101)
+    new Vector3(0.191857, -0.391061, 5.105533),
+    new Vector3(-1.348349, -1.079705, 5.427315),
+    new Vector3(-2.049685, -2.092055, 4.295254),
+    new Vector3(-2.405220, -3.711462, 3.846788)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(-1.414637, -12.111657, 0.977101),
-    new Vector3(-0.333386, -16.589966, 3.049517),
-    new Vector3(-2.088448, -22.079218, 0.033444),
+    new Vector3(-2.405220, -3.711462, 3.846788),
+    new Vector3(-4.182893, -11.808497, 1.604456),
+    new Vector3(-4.805010, -20.125534, 0.816464),
     new Vector3(2.311750, -24.817846, -0.300292)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
     new Vector3(2.311750, -24.817846, -0.300292),
-    new Vector3(5.146625, -26.582236, -0.515305),
-    new Vector3(9.112911, -26.292987, -0.446733),
+    new Vector3(5.081555, -26.644069, -0.734927),
+    new Vector3(9.122682, -26.297908, -0.458209),
     new Vector3(10.761617, -23.385043, -0.300292)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
     new Vector3(10.761617, -23.385043, -0.300292),
-    new Vector3(16.069046, -14.023939, 0.171125),
-    new Vector3(2.721774, -12.336981, 0.985883),
-    new Vector3(0.718990, 2.266700, 1.223782)
+    new Vector3(15.674695, -14.653080, 0.173100),
+    new Vector3(2.374672, -14.266444, 1.090044),
+    new Vector3(1.248781, 0.432172, 1.223782)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(0.718990, 2.266700, 1.223782),
-    new Vector3(0.494999, 3.899978, 1.250388),
-    new Vector3(0.178306, 4.438340, -1.675336),
-    new Vector3(-0.872541, 4.370861, -1.626841)
+    new Vector3(1.248781, 0.432172, 1.223782),
+    new Vector3(1.024790, 3.356407, 1.250388),
+    new Vector3(1.758823, 4.624824, -1.664127),
+    new Vector3(-0.541768, 4.114079, -1.839744)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(-0.872541, 4.370861, -1.626841),
-    new Vector3(-2.022139, 4.297040, -1.573789),
-    new Vector3(-1.591814, 2.838498, 0.298723),
-    new Vector3(-1.145646, 1.597480, 0.792350)
+    new Vector3(-0.541768, 4.114079, -1.839744),
+    new Vector3(-2.369873, 3.708228, -1.979293),
+    new Vector3(-1.621405, 2.836226, 1.175303),
+    new Vector3(-1.145646, 1.597480, 1.274631)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(-1.145646, 1.597480, 0.792350),
-    new Vector3(0.734123, -3.631097, 2.872066),
-    new Vector3(-0.155910, -4.823556, 0.126288),
-    new Vector3(2.767694, -8.935914, -0.300292)
+    new Vector3(-1.145646, 1.597480, 1.274631),
+    new Vector3(0.843552, -3.581847, 1.689927),
+    new Vector3(-0.295706, -4.823557, 1.265779),
+    new Vector3(2.627899, -8.935914, 1.112982)
 ));
 BowlineCurve_1.add(new CubicBezierCurve3(
-    new Vector3(2.767694, -8.935914, -0.300292),
-    new Vector3(5.312546, -12.515516, -0.671608),
+    new Vector3(2.627899, -8.935914, 1.112982),
+    new Vector3(5.180666, -12.526649, 0.979567),
+    new Vector3(12.045713, -21.700639, -1.914457),
+    new Vector3(14.969318, -25.812996, -2.341036)
+));
+
+export const BowlineCurve_0 = new CurvePath<Vector3>();
+
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-0.931381, 165.787743, -0.300292),
+    new Vector3(-0.799157, 101.565353, -0.535896),
+    new Vector3(-0.705596, 56.121586, -0.702610),
+    new Vector3(-0.592703, 1.288501, -0.903769)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-0.592703, 1.288501, -0.903769),
+    new Vector3(-0.623545, -1.146280, -0.903769),
+    new Vector3(-1.525114, -1.526919, 0.687887),
+    new Vector3(-2.185784, -3.496845, 1.110170)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-2.185784, -3.496845, 1.110170),
+    new Vector3(-2.504112, -4.446008, 1.313638),
+    new Vector3(-2.745400, -5.001954, 1.027534),
+    new Vector3(-3.022751, -5.975460, 1.165393)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-3.022751, -5.975460, 1.165393),
+    new Vector3(-3.303972, -6.962550, 1.305176),
+    new Vector3(-3.504292, -7.455983, 1.701020),
+    new Vector3(-3.603700, -8.480158, 1.819990)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-3.603700, -8.480158, 1.819990),
+    new Vector3(-3.718628, -9.664227, 1.957535),
+    new Vector3(-3.772185, -10.381016, 1.854273),
+    new Vector3(-3.510644, -11.545496, 1.755651)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-3.510644, -11.545496, 1.755651),
+    new Vector3(-2.262518, -17.102640, 1.285003),
+    new Vector3(-2.708483, -22.134793, 0.209283),
+    new Vector3(2.311750, -24.817846, -0.300292)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(2.311750, -24.817846, -0.300292),
+    new Vector3(5.250995, -26.388720, -0.598637),
+    new Vector3(9.122682, -26.297908, -0.458209),
+    new Vector3(10.761617, -23.385043, -0.300292)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(10.761617, -23.385043, -0.300292),
+    new Vector3(15.674695, -14.653080, 0.173100),
+    new Vector3(2.374672, -14.266444, 1.090044),
+    new Vector3(1.248781, 0.432172, 1.223782)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(1.248781, 0.432172, 1.223782),
+    new Vector3(1.024790, 3.356407, 1.250388),
+    new Vector3(1.758823, 4.624824, -1.664127),
+    new Vector3(-0.541768, 4.114079, -1.839744)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-0.541768, 4.114079, -1.839744),
+    new Vector3(-2.369873, 3.708228, -1.979293),
+    new Vector3(-1.621405, 2.836226, 1.175303),
+    new Vector3(-1.145646, 1.597480, 1.274631)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(-1.145646, 1.597480, 1.274631),
+    new Vector3(0.843552, -3.581847, 1.689927),
+    new Vector3(-0.295706, -4.823557, 1.265779),
+    new Vector3(2.627899, -8.935914, 1.112982)
+));
+BowlineCurve_0.add(new CubicBezierCurve3(
+    new Vector3(2.627899, -8.935914, 1.112982),
+    new Vector3(5.180666, -12.526649, 0.979567),
+    new Vector3(12.045713, -21.700639, -1.914457),
+    new Vector3(14.969318, -25.812996, -2.341036)
+));
+
+export const BowlineCurve_2 = new CurvePath<Vector3>();
+
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(-0.931381, 165.787743, -0.300292),
+    new Vector3(-0.799157, 101.565353, -0.535896),
+    new Vector3(-0.705596, 56.121586, -0.702610),
+    new Vector3(-0.592703, 1.288501, -0.903769)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(-0.592703, 1.288501, -0.903769),
+    new Vector3(-0.623545, -1.146280, -0.903769),
+    new Vector3(-3.185889, -1.022678, 2.438678),
+    new Vector3(0.776696, -1.241310, 2.314160)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(0.776696, -1.241310, 2.314160),
+    new Vector3(2.706820, -1.347803, 2.253508),
+    new Vector3(2.430355, -1.026193, 1.682643),
+    new Vector3(2.484064, -0.341943, 1.123538)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(2.484064, -0.341943, 1.123538),
+    new Vector3(2.534883, 0.305509, 0.594501),
+    new Vector3(1.842567, 0.898536, 0.588440),
+    new Vector3(1.046556, 1.075342, 0.396701)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(1.046556, 1.075342, 0.396701),
+    new Vector3(-0.297993, 1.373986, 0.072833),
+    new Vector3(-1.858318, 1.289858, 0.288062),
+    new Vector3(-2.405220, -0.012599, 0.208095)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(-2.405220, -0.012599, 0.208095),
+    new Vector3(-4.740422, -5.573929, -0.133355),
+    new Vector3(-5.783907, -19.191430, -0.161324),
+    new Vector3(2.311750, -24.817846, -0.300292)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(2.311750, -24.817846, -0.300292),
+    new Vector3(5.059089, -26.727224, -0.347452),
+    new Vector3(9.122682, -26.297908, -0.458209),
+    new Vector3(10.761617, -23.385043, -0.300292)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(10.761617, -23.385043, -0.300292),
+    new Vector3(15.674695, -14.653080, 0.173100),
+    new Vector3(2.374672, -14.266444, 1.090044),
+    new Vector3(1.248781, 0.432172, 1.223782)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(1.248781, 0.432172, 1.223782),
+    new Vector3(1.024790, 3.356407, 1.250388),
+    new Vector3(1.758823, 4.624824, -1.664127),
+    new Vector3(-0.541768, 4.114079, -1.839744)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(-0.541768, 4.114079, -1.839744),
+    new Vector3(-2.369873, 3.708228, -1.979293),
+    new Vector3(-1.621405, 2.836226, 1.175303),
+    new Vector3(-1.145646, 1.597480, 1.274631)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(-1.145646, 1.597480, 1.274631),
+    new Vector3(0.843552, -3.581847, 1.689927),
+    new Vector3(-0.295706, -4.823557, 1.265779),
+    new Vector3(2.627899, -8.935914, 1.112982)
+));
+BowlineCurve_2.add(new CubicBezierCurve3(
+    new Vector3(2.627899, -8.935914, 1.112982),
+    new Vector3(5.180666, -12.526649, 0.979567),
     new Vector3(12.045713, -21.700639, -1.914457),
     new Vector3(14.969318, -25.812996, -2.341036)
 ));
