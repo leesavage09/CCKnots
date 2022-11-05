f792bd46576ae1928996ed27b7d7dd2c534e85f8
leesavage09 <leesavage09@gmail.com>
2022-11-05 12:32:35 +0200

    updated knot interface and added tweenCurves

--- src/components/rope/knot.tsx
+++ src/components/rope/knot.tsx
@@ -2,53 +2,64 @@ import { MeshProps, useThree } from "@react-three/fiber"
 import React, { useEffect, useState } from "react"
 import { Flow } from "three/examples/jsm/modifiers/CurveModifier.js";
 import { ropeMesh } from "./ropeMesh";
-import { BowlineCurve } from "./bowlineCurve";
 import { CurvePath, Mesh, Vector3 } from "three";
+import { tweenCurves } from "../support";
 
-interface KnotProps extends MeshProps {
-    min: number
-    max: number
-    animationFrame: number
-    curve: CurvePath<Vector3>
+export interface KnotProps extends MeshProps {
+    moveMin: number
+    moveMax: number
+    moveKeyFrame: number
+    bendKeyFrame?: number
+    curves: [CurvePath<Vector3>,CurvePath<Vector3>]
     mesh: Mesh
-    outline: boolean
+    debugOutline: boolean
 }
 
-export const Knot: React.FC<KnotProps> = ({ min, max, animationFrame, curve, mesh, outline, ...props }) => {
+export const Knot: React.FC<KnotProps> = ({ moveMin, moveMax, moveKeyFrame, bendKeyFrame, curves, mesh, debugOutline, ...props }) => {
     const three = useThree();
     const [flow, setFlow] = useState<Flow>()
 
     useEffect(() => {
         if (!flow) return
-
-        const diff = max - min
-        const newPosition = min + animationFrame * diff
+        const diff = moveMax - moveMin
+        const newPosition = moveMin + moveKeyFrame * diff
         const newShift = 0 - flow.uniforms.pathOffset.value + newPosition
         flow.moveAlongCurve(newShift)
-    }, [flow, animationFrame])
+    }, [flow, moveKeyFrame])
+
+    useEffect(() => {
+        if (!flow || !bendKeyFrame) return
+        flow.updateCurve(0, tweenCurves(curves[0], curves[1], bendKeyFrame));
+    }, [flow, bendKeyFrame])
+
 
     useEffect(() => {
         const flow = new Flow(ropeMesh);
-        flow.updateCurve(0, BowlineCurve);
-        three.scene.add(flow.object3D);
-        flow.moveAlongCurve(min)
         setFlow(flow)
-
+        three.scene.add(flow.object3D);
         return () => {
             three.scene.remove(flow.object3D)
         }
     }, [])
 
-    if (outline) {
+    if (debugOutline) {
         return (
+            <>
             <mesh
                 {...props}
-                receiveShadow
                 position={[0, 0, 0]}
             >
-                <tubeGeometry args={[BowlineCurve, 500, 0.2, 4, false]} />
-                <meshStandardMaterial wireframe />
+                <tubeGeometry args={[curves[0], 500, 0.2, 4, false]} />
+                <meshStandardMaterial wireframe color={'red'} />
             </mesh>
+            <mesh
+                {...props}
+                position={[0, 0, 0]}
+            >
+                <tubeGeometry args={[curves[1], 500, 0.2, 4, false]} />
+                <meshStandardMaterial wireframe color={'blue'}/>
+            </mesh>
+            </>
         )
     }
     return null
