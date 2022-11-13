d2fc63d876bb3ff14b06e099a41361bdec0d4595
leesavage09 <leesavage09@gmail.com>
2022-11-13 11:39:23 +0200

    added and implemented menu on homepage

---
+++ src/components/knotCard.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+interface KnotCardProps {
+    image: string,
+    title: string,
+    body: string
+    action: () => void
+}
+
+export const KnotCard: React.FC<KnotCardProps> = ({ image, title, body, action }) => {
+    return (
+        <Card>
+            <CardActionArea
+                onClick={action}
+            >
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={image}
+                    alt={title}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {body}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,217 +1,76 @@
-import React, { useState, useEffect } from "react";
-import { IonSlide, IonContent } from '@ionic/react';
-import { Canvas } from '@react-three/fiber'
-import { Bowline } from "../components/rope/knots/bowline";
-import { Sky } from "../components/sky";
-import { SheetBend } from "../components/rope/knots/sheetBend";
-import { ArcballControl } from "../components/arcballControls";
-import { ReefKnot } from "../components/rope/knots/reefKnot";
-import { DoubleSheetBend } from "../components/rope/knots/doubleSheetBend";
-import { CloveHitch } from "../components/rope/knots/cloveHitch";
-import { RoundTurn } from "../components/rope/knots/roundTurn";
+import { IonContent, IonSlide } from '@ionic/react'
+import { Container, Stack, Typography } from '@mui/material'
+import { useHistory } from 'react-router'
+import { AppBar } from '../components/appBar'
+import { KnotCard } from '../components/knotCard'
 
-import "./Home.css";
+export const Home = () => {
+    const history = useHistory()
 
-export const Home: React.FC = () => {
-  const [animationSlider, setAnimationSlider] = useState("1000")
-  const [animation, setAnimation] = useState(0)
+    return (
+        <>
+            <AppBar />
+            <IonSlide>
+                <IonContent>
+                    <Typography variant="h1" gutterBottom>
+                        Knots
+                    </Typography>
+                    <Container maxWidth="xs">
+                        <Stack spacing={4}>
+                            <KnotCard
+                                image={'/assets/knots/cloveHitch.png'}
+                                title={'Clove Hitch'}
+                                body={'Clove Hitch ...'}
+                                action={() => {
+                                    history.push('/knots/cloveHitch');
+                                }}
+                            />
+                            <KnotCard
+                                image={'/assets/knots/roundTurn.png'}
+                                title={'Round Turn & Two Half Hitches'}
+                                body={'Round Turn & Two Half Hitches ...'}
+                                action={() => {
+                                    history.push('/knots/roundTurn');
+                                }}
+                            />
+                            <KnotCard
+                                image={'/assets/knots/reefKnot.png'}
+                                title={'Reef Knot'}
+                                body={'Reef Knot ...'}
+                                action={() => {
+                                    history.push('/knots/reefKnot');
+                                }}
+                            />
+                            <KnotCard
+                                image={'/assets/knots/bowline.png'}
+                                title={'Bowline'}
+                                body={'Bowline ...'}
+                                action={() => {
+                                    history.push('/knots/bowline');
+                                }}
+                            />
+                            <KnotCard
+                                image={'/assets/knots/sheetBend.png'}
+                                title={'Sheet Bend'}
+                                body={'Sheet Bend ...'}
+                                action={() => {
+                                    history.push('/knots/sheetBend');
+                                }}
+                            />
+                            <KnotCard
+                                image={'/assets/knots/doubleSheetBend.png'}
+                                title={'Double Sheet Bend'}
+                                body={'Double Sheet Bend ...'}
+                                action={() => {
+                                    history.push('/knots/doubleSheetBend');
+                                }}
+                            />
+                        </Stack>
+                        <div style={{ height: 100 }} />
 
-  useEffect(() => {
-    setAnimation((parseInt(animationSlider) / 1000))
-  }, [animationSlider])
-
-  return (
-    <IonSlide>
-      <IonContent>
-        <div className="full-height">
-          <h1>Round Turn</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 20] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <RoundTurn frame={animation} />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div>
-
-
-        <div className="full-height">
-          <h1>Clove</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 20] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <CloveHitch frame={animation} />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div>
-
-        <div className="full-height">
-          <h1>Bowline</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 50] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <Bowline frame={animation} />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div>
-
-        <div className="full-height">
-          <h1>Sheet Bend</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 50] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <SheetBend frame={animation} />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div>
-
-
-
-        <div className="full-height">
-          <h1>Sheet Bend useAlternativeTying</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 50] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <SheetBend frame={animation} useAlternativeTying />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div>
-
-
-        <div className="full-height">
-          <h1>DB Sheet</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 20] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <DoubleSheetBend frame={animation} />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div>
-
-        <div className="full-height">
-          <h1>DB Sheet ALT</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 20] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <DoubleSheetBend frame={animation} useAlternativeTying />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div>
-
-
-        <div className="full-height">
-          <h1>Reef</h1>
-          <Canvas
-            shadows
-            camera={{ fov: 45, position: [0, 0, 20] }}
-          >
-            <Sky />
-            <ArcballControl />
-            <ambientLight intensity={0.3} />
-            <pointLight castShadow position={[10, 10, 10]} intensity={0.7} />
-            <ReefKnot frame={animation} />
-
-          </Canvas>
-          <input
-            className="input"
-            onChange={(e) => setAnimationSlider(e.target.value)}
-            type="range"
-            min="0"
-            value={animationSlider}
-            max="1000"
-          />
-        </div >
-      </IonContent>
-    </IonSlide>
-  )
+                    </Container>
+                </IonContent>
+            </IonSlide>
+        </>
+    )
 }
