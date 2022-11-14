3e2ea88742cf6414f3e6e6df5211c317bae6ac16
leesavage09 <leesavage09@gmail.com>
2022-11-14 17:03:22 +0200

    homepage uses dynamic generator for knots

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,22 @@ import { Container, Stack, Typography } from '@mui/material'
 import { useHistory } from 'react-router'
 import { AppBar } from '../components/appBar'
 import { KnotCard } from '../components/knotCard'
+import { knots } from '../knots'
 
 export const Home = () => {
     const history = useHistory()
+
+    const knotCards = knots.map((knot) => (
+        <KnotCard
+            key={knot.name}
+            image={knot.image}
+            title={knot.name}
+            body={knot.description}
+            action={() => {
+                history.push(knot.url);
+            }}
+        />
+    ))
 
     return (
         <>
@@ -17,54 +30,7 @@ export const Home = () => {
                     </Typography>
                     <Container maxWidth="xs">
                         <Stack spacing={4}>
-                            <KnotCard
-                                image={'/assets/knots/cloveHitch.png'}
-                                title={'Clove Hitch'}
-                                body={'Clove Hitch ...'}
-                                action={() => {
-                                    history.push('/knots/cloveHitch');
-                                }}
-                            />
-                            <KnotCard
-                                image={'/assets/knots/roundTurn.png'}
-                                title={'Round Turn & Two Half Hitches'}
-                                body={'Round Turn & Two Half Hitches ...'}
-                                action={() => {
-                                    history.push('/knots/roundTurn');
-                                }}
-                            />
-                            <KnotCard
-                                image={'/assets/knots/reefKnot.png'}
-                                title={'Reef Knot'}
-                                body={'Reef Knot ...'}
-                                action={() => {
-                                    history.push('/knots/reefKnot');
-                                }}
-                            />
-                            <KnotCard
-                                image={'/assets/knots/bowline.png'}
-                                title={'Bowline'}
-                                body={'Bowline ...'}
-                                action={() => {
-                                    history.push('/knots/bowline');
-                                }}
-                            />
-                            <KnotCard
-                                image={'/assets/knots/sheetBend.png'}
-                                title={'Sheet Bend'}
-                                body={'Sheet Bend ...'}
-                                action={() => {
-                                    history.push('/knots/sheetBend');
-                                }}
-                            />
-                            <KnotCard
-                                image={'/assets/knots/doubleSheetBend.png'}
-                                title={'Double Sheet Bend'}
-                                body={'Double Sheet Bend ...'}
-                                action={() => {
-                                    history.push('/knots/doubleSheetBend');
-                                }}
-                            />
+                            {knotCards}
                         </Stack>
                         <div style={{ height: 100 }} />
                     </Container>
