77217f560efbe353ca402c800714d0ef3d3a2a82
leesavage09 <leesavage09@gmail.com>
2022-11-14 15:40:57 +0200

    fixed animation bug for bowline

--- src/components/threejs/rope/knots/bowline.tsx
+++ src/components/threejs/rope/knots/bowline.tsx
@@ -6,25 +6,33 @@ import { useRopeMaterial, whiteTexture } from '../../hooks/useRopeMaterial'
 import { Rope } from '../rope'
 
 export const Bowline: React.FC<AnimatedKnotProps> = ({ frame }) => {
-    const { moveFrame, curveFrame } = useRopeAnimation(frame, [{
-        frames: [0, 0.200],
-        values: [0, 0.260]
-    }, {
-        frames: [0.700, 1],
-        values: [0.260, 1]
-    }], [{
-        frames: [0, 0.200],
-        values: [0, 0]
-    }, {
-        frames: [0.200, 0.600],
-        values: [0, 0.5]
-    }, {
-        frames: [0.600, 0.700],
-        values: [0.5, 1]
-    }, {
-        frames: [0.700, 1],
-        values: [1, 1]
-    }])
+    const { moveFrame, curveFrame } = useRopeAnimation(
+        frame,
+
+        [{
+            frames: [0, 0.200],
+            values: [0, 0.260]
+        }, {
+            frames: [0.200, 0.700],
+            values: [0.260, 0.260]
+        }, {
+            frames: [0.700, 1],
+            values: [0.260, 1]
+        }],
+
+        [{
+            frames: [0, 0.200],
+            values: [0, 0]
+        }, {
+            frames: [0.200, 0.600],
+            values: [0, 0.5]
+        }, {
+            frames: [0.600, 0.700],
+            values: [0.5, 1]
+        }, {
+            frames: [0.700, 1],
+            values: [1, 1]
+        }])
 
     return (
         <Rope
