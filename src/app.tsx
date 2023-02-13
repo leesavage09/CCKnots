930df9ae7a962cf4d12bfc77a2992b8a84485b56
leesavage09 <leesavage09@gmail.com>
2023-02-13 15:25:13 +0200

    added backHandler and app title

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
-import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { Home } from './pages/home';
+import { Routes } from './routes';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -16,34 +15,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { knots } from './knots';
-
 
 setupIonicReact();
 
-const App: React.FC = () => {
-    const knotRoutes = knots.map((knot) => (
-        <Route key={knot.url} exact path={knot.url}>
-            {knot.pageComponent}
-        </Route>
-    ))
-
-    return (
-        <IonApp>
-            <IonReactRouter>
-                <IonRouterOutlet>
-                    <Route exact path="/home">
-                        <Home />
-                    </Route>
-                    <Route exact path="/">
-                        <Redirect to="/home" />
-                    </Route>
-                    {knotRoutes}
-                </IonRouterOutlet>
-            </IonReactRouter>
-        </IonApp>
-    )
-}
-
+const App: React.FC = () => (
+    <IonApp>
+        <IonReactRouter>
+            <IonRouterOutlet>
+                <Routes />
+            </IonRouterOutlet>
+        </IonReactRouter>
+    </IonApp>
+)
 
 export default App;
