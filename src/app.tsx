0718422efc3d35177824447a78d0cb41d314d061
leesavage09 <leesavage09@gmail.com>
2023-03-05 16:59:57 +0200

    added list styles

--- src/app.tsx
+++ src/app.tsx
@@ -16,6 +16,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import './app.css';
+
 setupIonicReact();
 
 const App: React.FC = () => (
