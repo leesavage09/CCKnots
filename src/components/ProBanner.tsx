611e407c7ae56a77b97b36a418248a81f0e00303
Lee <leesavage09@gmail.com>
2023-08-01 15:43:44 +0300

    updated text

--- src/components/ProBanner.tsx
+++ src/components/ProBanner.tsx
@@ -3,28 +3,16 @@ import { Typography } from "@mui/material";
 import { PaperP } from "./knotPageTemplate";
 import { useAppState } from "./context/appContext";
 
-export const DonationBanner = () => {
+export const ProBanner = () => {
   const { pro } = useAppState();
 
-  if (!pro) return <></>;
+  if (!pro || pro.owned) return <></>;
 
   if (pro.isOrdering)
     return (
       <PaperP>
         <Typography variant="body1" component="p" gutterBottom>
           Your order for Sailing Knots 3D Pro is now processing!
-        </Typography>
-      </PaperP>
-    );
-
-  if (pro.owned)
-    return (
-      <PaperP>
-        <Typography variant="subtitle2" component="h6" gutterBottom>
-          Thank you
-        </Typography>
-        <Typography variant="body1" component="p" gutterBottom>
-          You now have Sailing Knots 3D Pro!
         </Typography>
       </PaperP>
     );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,10 +3,12 @@ import { Container, List, Typography } from "@mui/material";
 import { AppBar } from "../components/appBar";
 import { KnotListItem } from "../components/knotListItem";
 import { knots } from "../knots";
-import { DonationBanner } from "../components/DonationBanner";
+import { ProBanner } from "../components/ProBanner";
 import { PaperP } from "../components/knotPageTemplate";
+import { useAppState } from "../components/context/appContext";
 
 export const Home = () => {
+  const { pro } = useAppState();
   const knotCards = knots.map((knot) => (
     <KnotListItem knot={knot} large={true} />
   ));
@@ -17,15 +19,26 @@ export const Home = () => {
       <IonSlide>
         <IonContent>
           <Container sx={{ marginTop: 8, marginBottom: 6 }} maxWidth="xs">
-            <DonationBanner />
+            <ProBanner />
             <List
               sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
             >
               {knotCards}
             </List>
-            <PaperP>              
+            <PaperP>
+              {pro?.owned && (
+                <>
+                  <Typography variant="subtitle1" component="h6" gutterBottom>
+                    Thank you for purchasing
+                  </Typography>
+                  <Typography variant="subtitle2" component="h6" gutterBottom>
+                    Sailing Knots 3D Pro!
+                  </Typography>
+                </>
+              )}
               <Typography variant="body1" component="p" gutterBottom>
-                Please consider leaving a review. Reviews will encorage futher development
+                Please consider leaving a review. Reviews will encorage futher
+                development
               </Typography>
               <IonButton
                 color="secondary"
